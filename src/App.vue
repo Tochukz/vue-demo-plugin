3e8794dc3ff1a6fd8bc413a2350f782ba4512b4e
Chucks Nwachukwu <chucks@bbd.co.za>
2020-07-12 21:07:16 +0200

    exported App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,15 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Banner><img alt="Vue logo" src="./assets/logo.png"></Banner>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Banner from './components/Banner';
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Banner
   }
 }
 </script>
